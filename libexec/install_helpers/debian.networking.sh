a010e2961147fa08c5c1da13caa732e50243d980
Guillaume ZITTA <lxc@zitta.fr>
2010-01-14 08:50:29 +0100

    added IP address detection by hostname resolution

--- libexec/install_helpers/debian.networking.sh
+++ libexec/install_helpers/debian.networking.sh
@@ -4,7 +4,14 @@
 . ${lxc_GLOBAL_FUNCTIONS}
 . ${lxc_TEMPLATE_NETCONF} || die "enable to load ${lxc_TEMPLATE_NETCONF}"
 
-needed_var_check "lxc_CONTAINER_ROOTFS lxc_CONTAINER_IP lxc_TEMPLATE_NETCONF lxc_CONTAINER_GATEWAY lxc_CONTAINER_NETMASK lxc_CONTAINER_MTU lxc_CONTAINER_NAME"
+needed_var_check "lxc_CONTAINER_ROOTFS lxc_TEMPLATE_NETCONF lxc_CONTAINER_GATEWAY lxc_CONTAINER_NETMASK lxc_CONTAINER_MTU lxc_CONTAINER_NAME"
+
+if [[ -z ${lxc_CONTAINER_IP} ]]
+then
+	lxc_CONTAINER_IP=$(getent hosts ${lxc_CONTAINER_NAME} | awk 'NR==1 { print $1 }')
+	[[ "x${lxc_CONTAINER_IP}" == "x" ]] && die "IP address not provided and enable to find it from hostname"
+	echo "Found IP address ${lxc_CONTAINER_IP}"
+fi
 
 [[ -d ${lxc_CONTAINER_ROOTFS}/etc/network ]] || die "enable to find ${lxc_CONTAINER_ROOTFS}/etc/network"
 
